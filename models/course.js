cb3553e21725688cb0f58d4391c5cfe36ee7a54d
Valentin <primarh111@mail.ru>
2020-10-21 12:08:28 +0300

    add change corse functional

--- models/course.js
+++ models/course.js
@@ -14,7 +14,7 @@ class Course {
   toJSON () {
       return {
         title: this.title,
-        rice: this.price,
+        price: this.price,
         img:this.img,
         id: this.id
       }
@@ -47,8 +47,30 @@ class Course {
             }
           );
     })
-   
   }
+  static async getById (id) {
+     const courses = await Course.getAll()
+
+     return courses.find(item => item.id === id)
+  }
+
+  static async update (course) {
+    const courses = await Course.getAll()
+
+    const courseIndex = courses.findIndex( item => item.id === course.id)
+    courses[courseIndex] = course
+
+    return new Promise((resolve, reject) => {
+      fs.writeFile(path.join(__dirname, "..", "data", "courses.json"), JSON.stringify(courses), (err)=> {
+          if(err) {
+              reject( err)
+          } else {
+              resolve ()
+          }
+      })
+  })
+  }
+
 }
 
 module.exports = Course
--- routes/courses.js
+++ routes/courses.js
@@ -13,4 +13,25 @@ router.get('/',async (req, res) => {
     })
 })
 
+router.get('/:id/edit', async (req, res) => {
+
+    const course = await Course.getById(req.params.id)
+    res.render('course-edit', {
+        course: course
+    })
+})
+
+router.post('/edit', async (req, res) => {
+    await Course.update(req.body)
+    res.redirect('/courses')
+})
+
+router.get('/:id', async (req, res) => {
+    const course = await Course.getById(req.params.id)
+    res.render('course', {
+        layout: 'empty',
+        course: course
+    })
+})
+
 module.exports = router
